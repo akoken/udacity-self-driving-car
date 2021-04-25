37a67f9083f757e0cc0ff9f5a5f3d0dae707c6e4
Abdurrahman Köken <abdurrahman@akoken.com>
2021-04-26 00:42:13 +0300

    remove unnecessary files.

--- src/CarND-Advanced-Lane-Lines/main.py
+++ src/CarND-Advanced-Lane-Lines/main.py
@@ -7,8 +7,8 @@ def pipeline(img):
 
 if __name__=='__main__':
     lane = Lane()
-    selector = "project"
-    output_video = 'out_{}_video.mp4'.format(selector)
-    clip = VideoFileClip('{}_video.mp4'.format(selector))
+    video_name = "project"
+    output_video = 'out_{}_video.mp4'.format(video_name)
+    clip = VideoFileClip('{}_video.mp4'.format(video_name))
     video_clip = clip.fl_image(pipeline)
     video_clip.write_videofile(output_video, audio=False)
